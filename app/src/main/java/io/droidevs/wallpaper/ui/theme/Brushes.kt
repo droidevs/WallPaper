27bf070229d103b9a7ccff155a20f074f7baf821
Mouad Oumous <mouadoumous@gmail.com>
2025-01-29 13:37:22 +0100

    add edge to edge compose custom setup

---
+++ app/src/main/java/io/droidevs/wallpaper/ui/theme/Brushes.kt
@@ -0,0 +1,32 @@
+package io.droidevs.wallpaper.ui.theme
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val backgroundGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFF0C0E0E),
+        Color(0xFF465353)
+    )
+)
+
+val orangeYellowGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFFff8300),  //0xFFff8300
+        Color(0xFFfff500)
+    )
+)
+
+val yellowOrangeGradient = Brush.horizontalGradient(
+    colors = listOf(
+        Color(0xFFfff500),  //0xFFff8300
+        Color(0xFFff8300)
+    )
+)
+
+val blueYellowGradient = Brush.horizontalGradient(
+    colors = listOf(
+        Color(0xFF0ffff0),
+        Color(0xFFfeff01)
+    )
+)
