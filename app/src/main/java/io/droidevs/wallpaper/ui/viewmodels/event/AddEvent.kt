4925dbb7fb76c5dae46cb706d838e01352a9f629
Mouad Oumous <mouadoumous@gmail.com>
2025-02-02 15:20:01 +0100

    add events for moving wallpapers

--- app/src/main/java/io/droidevs/wallpaper/ui/viewmodels/event/AddEvent.kt
+++ app/src/main/java/io/droidevs/wallpaper/ui/viewmodels/event/AddEvent.kt
@@ -6,36 +6,22 @@ import io.droidevs.wallpaper.domain.Wallpaper
 
 sealed class AddEvent {
 
-    data class AddWallpaperToAlbum(val wallpaper: Wallpaper, val album: Album) : AddEvent() {
-        fun getDetails() = "Adding wallpaper '${wallpaper.name}' to album '${album.title}'"
-    }
-
-    data class AddWallpapersToAlbum(val wallpapers: List<Wallpaper>, val album: Album) : AddEvent() {
-        fun getDetails() = "Adding ${wallpapers.size} wallpapers to album '${album.title}'"
-    }
-
     data class AddAlbum(val album: Album) : AddEvent() {
         fun getDetails() = "Adding new album '${album.title}'"
     }
 
-    data class AddAlbumWithWallpaper(val album: Album, val wallpaper: Wallpaper) : AddEvent() {
-        fun getDetails() = "Creating album '${album.title}' with wallpaper '${wallpaper.name}'"
+    data class AddAlbumWithWallpaper(val album: Album, val wallpaperUri: String) : AddEvent() {
+        fun getDetails() = "Creating album '${album.title}' with wallpaper uri '${wallpaperUri}'"
     }
 
-    data class AddAlbumWithWallpapers(val album: Album, val wallpapers: List<Wallpaper>) : AddEvent() {
-        fun getDetails() = "Creating album '${album.title}' with ${wallpapers.size} wallpapers"
-    }
-
-    data class AddAlbumWithNewWallpapers(val album: Album, val wallpapersUri: List<String>) : AddEvent() {
+    data class AddAlbumWithWallpapers(val album: Album, val wallpapersUri: List<String>) : AddEvent() {
         fun getDetails() = "Creating album '${album.title}' with ${wallpapersUri.size} new wallpapers"
     }
 
     companion object {
-        fun fromWallpaper(wallpaper: Wallpaper, album: Album) = AddWallpaperToAlbum(wallpaper, album)
-        fun fromWallpapers(wallpapers: List<Wallpaper>, album: Album) = AddWallpapersToAlbum(wallpapers, album)
         fun fromNewAlbum(album: Album) = AddAlbum(album)
-        fun fromAlbumWithWallpaper(album: Album, wallpaper: Wallpaper) = AddAlbumWithWallpaper(album, wallpaper)
-        fun fromAlbumWithWallpapers(album: Album, wallpapers: List<Wallpaper>) = AddAlbumWithWallpapers(album, wallpapers)
-        fun fromNewWallpapers(album: Album, wallpapersUri: List<String>) = AddAlbumWithNewWallpapers(album, wallpapersUri)
+        fun fromAlbumWithWallpaper(album: Album, wallpaper: String) = AddAlbumWithWallpaper(album, wallpaper)
+        fun fromAlbumWithWallpapers(album: Album, wallpapers: List<String>) = AddAlbumWithWallpapers(album, wallpapers)
+
     }
 }
