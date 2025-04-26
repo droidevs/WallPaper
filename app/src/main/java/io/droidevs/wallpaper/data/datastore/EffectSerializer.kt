2196d4076c9bd9a1acf7d9276ffceddb7d4b5984
Mouad Oumous <mouadoumous@gmail.com>
2025-04-26 20:34:25 +0200

    set up local preferences

---
+++ app/src/main/java/io/droidevs/wallpaper/data/datastore/EffectSerializer.kt
@@ -0,0 +1,32 @@
+package io.droidevs.wallpaper.data.datastore
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import io.droidevs.wallpaper.models.Effect
+import io.droidevs.wallpaper.models.WallpaperEffects
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+
+object WallpaperEffectsSerializer : Serializer<WallpaperEffects> {
+    override val defaultValue: WallpaperEffects = WallpaperEffects()
+
+    override suspend fun readFrom(input: InputStream): WallpaperEffects {
+        return try {
+            Json.decodeFromString(
+                WallpaperEffects.serializer(),
+                input.readBytes().decodeToString()
+            )
+        } catch (e: SerializationException) {
+            throw CorruptionException("Cannot read WallpaperEffects", e)
+        }
+    }
+
+    override suspend fun writeTo(t: WallpaperEffects, output: OutputStream) {
+        output.write(
+            Json.encodeToString(WallpaperEffects.serializer(), t).encodeToByteArray()
+        )
+    }
+}
+
