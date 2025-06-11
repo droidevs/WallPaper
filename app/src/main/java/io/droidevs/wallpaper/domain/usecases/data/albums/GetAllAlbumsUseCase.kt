e7aa6a1fd474043f24016001be9fe5546a7a2533
Mouad Oumous <mouadoumous@gmail.com>
2025-06-11 16:07:05 +0200

    enhance app

--- app/src/main/java/io/droidevs/wallpaper/domain/usecases/data/albums/GetAllAlbumsUseCase.kt
+++ app/src/main/java/io/droidevs/wallpaper/domain/usecases/data/albums/GetAllAlbumsUseCase.kt
@@ -1,16 +1,20 @@
 package io.droidevs.wallpaper.domain.usecases.data.albums
 
+import io.droidevs.wallpaper.dispatchers.AppDispatchers
 import io.droidevs.wallpaper.domain.Album
 import io.droidevs.wallpaper.domain.repository.AlbumRepository
 import io.droidevs.wallpaper.domain.result.Result
 import io.droidevs.wallpaper.domain.result.errors.DatabaseError
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 
 class GetAllAlbumsUseCase (
-    private val albumRepository: AlbumRepository
+    private val albumRepository: AlbumRepository,
+    private val dispatchers: AppDispatchers
 ){
 
     suspend fun invoke() : Flow<Result<List<Album>, DatabaseError>> {
-
+        return albumRepository.getAllAlbums()
+            .flowOn(dispatchers.io)
     }
 }
